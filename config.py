b0bb25b0b64d6876e0f46893be4fc52014feb768
manicaraguaMM <118110924+manicaraguaMM@users.noreply.github.com>
2022-11-20 13:06:38 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','Rebelde59').split(';')
 
-static_proxy = 'socks5://KHDKKGYGJDJEGKYHJDGFGGYJIJIDRDGGLGGJKH' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
+static_proxy = 'socks5h://KHDKKGYGJDJEGKYHJDGFGGYJIJIDRDGGLGGJKH' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
 PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
