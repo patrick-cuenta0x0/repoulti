2fcea5319eb319c9438eaeeb67b43920c47105d0
patrick-cuenta0x0 <118947967+patrick-cuenta0x0@users.noreply.github.com>
2022-11-23 20:58:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ from pydownloader.downloader import Downloader
 import shorturl
 import xdlink
 
-tl_admin_users = ['lil_l4chy',] #Poner aqui los user con acceso permanente
-godlist = ['lil_l4chy'] #Poner aqui los admin 
+tl_admin_users = ['nuevoss01',] #Poner aqui los user con acceso permanente
+godlist = ['nuevoss01'] #Poner aqui los admin 
 
 async def get_root(username):
     if os.path.isdir(config.ROOT_PATH+username)==False:
